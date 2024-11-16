ba240a62352f4868abcc9d276be7294807a778ac
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-11-16 12:27:38 +0530

    completed project

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
 import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import CreateTask from "./pages/tasks/create";
+import EditTask from "./pages/tasks/edit";
 
 function App() {
   return (
@@ -62,6 +65,15 @@ function App() {
                       <Route index element={<CompanyList />} />
                       <Route path="new" element={<Create />} />
                       <Route path="edit/:id" element={<Edit />} />
+                    </Route>
+                    <Route path="/tasks" element={
+                    <List>
+                      <Outlet />
+                    </List>
+                  }>
+                    <Route path="new" element={<CreateTask />} />
+                    <Route path="edit/:id" element={<EditTask />} />
+                      
                     </Route>
                   </Route>
                 </Routes>
