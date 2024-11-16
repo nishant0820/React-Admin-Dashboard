c42c416d868a4aba6f508aa520ca8c8372a37a76
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-11-16 19:25:29 +0530

    completed project

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -10,6 +10,7 @@ import { TasksQuery, TaskStagesQuery } from '@/graphql/types'
 import { DragEndEvent } from '@dnd-kit/core'
 import { useList, useNavigation, useUpdate } from '@refinedev/core'
 import { GetFieldsFromList } from '@refinedev/nestjs-query'
+import React from 'react'
 
 type Task = GetFieldsFromList<TasksQuery>
 type TaskStage = GetFieldsFromList<TaskStagesQuery> & { tasks: Task[] }
